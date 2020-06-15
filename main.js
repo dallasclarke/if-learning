0338afc68f1bf3c18a167172b299f7c37bdefa9a
Dallas Clarke <dallas.clarke@codeimmersives.com>
2020-06-15 12:08:32 -0700

    lab

--- main.js
+++ main.js
@@ -2,8 +2,45 @@
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
 
+function isItLong(str) {
+  if (str.length > 20) {
+    return "That's a long string!"
+  }
+}
 
+function isItMedium(str) {
+  if (str.length >= 10 && str.length <= 20) {
+    return "That's a regular sized string!"
+  }
+}
 
+function isItShort(str) {
+  if (str.length < 10) {
+    return "That's a small string!"
+  } else (str.length > 10) 
+    return "That's not a small string!"
+  
+}
+
+function howLongIsMyString(str) {
+  if (str.length > 20) {
+    return "That's a long string!" }
+    if (str.length === 20) {
+      return "That's a regular sized string!"
+    } else if (str.length > 10 && str.length < 20) {
+    return "That's a regular sized string!" }
+    else if (str.length === 10) {
+      return "That's a regular sized string!" 
+    }
+    else if (str.length < 10) {
+      return "That's a small string!" }
+    }
+  
+    function instructorHeight (nameOne, nameTwo) {
+      if (nameOne) {
+        return '62' 
+    }
+  }
 
 
 
